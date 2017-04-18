a4261dd15f2b525460be996a85e3b9d8e93a70d0
danganhnguyen <danganhnguyen199x@gmail.com>
2017-04-18 18:21:04 +0700

    branch Anh

--- User/NguyenDangAnh/table/untitled.py
+++ User/NguyenDangAnh/table/untitled.py
@@ -1,12 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,json
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('demo.html')
+    temp=[x for x in  range(10)]
+    return render_template('demo.html',temp=temp)
+
+
+@app.route('/about',methods='GET','POST')
+def aboutPage():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+#ten dang nhap su dung post
+#con mac dinh dung get
