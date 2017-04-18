6b3145e24cd6b1e9d167343becb04ead12ea453a
ducdan_ttcnpm <51200482@hcmut.edu.vn>
2017-04-18 21:38:10 +0700

    [DanHD] update project

--- User/DanHD/fruitandgreen_project/fruitandgreen_project.py
+++ User/DanHD/fruitandgreen_project/fruitandgreen_project.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__,static_folder='vendors')
 
@@ -7,6 +7,19 @@ app = Flask(__name__,static_folder='vendors')
 def hello_world():
     return render_template('index.html')
 
+@app.route('/nhapkho')
+def nhapkho():
+    return render_template('nhapkho.html')
 
+@app.route('/nhap',methods=['POST'])
+def nhap():
+    fistname=request.form['firstname']
+    lastname = request.form['lastname']
+    print(fistname+" "+lastname)
+    dic={
+        'first':fistname,
+        'last':lastname
+    }
+    return render_template('nhapkho.html',fist=dic)
 if __name__ == '__main__':
     app.run(debug=True)
